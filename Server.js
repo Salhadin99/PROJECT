0bf4f0fb1af51d1f90075e88f0eca5ea40cecc01
salhadin99 <usmansalhadin@gmail.com>
2023-12-29 14:42:46 +0300

    first commit

--- Server.js
+++ Server.js
@@ -1,68 +1,159 @@
-const express = require('express');
-const cors = require('cors');
-const fetch = require('node-fetch');
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
-const app = express();
-const port = 5000;
+// app.use(express.static('public'));
 
-app.use(cors());
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
 
-app.get('/api/countries/:name', async (req, res) => {
-  const countryName = req.params.name;
-
-  try {
-    const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
-    const data = await response.json();
-
-    if (data.length > 0) {
-      res.json(data[0]);
-    } else {
-      res.status(404).json({ error: 'Country not found' });
-    }
-  } catch (error) {
-    console.error('Error fetching country data:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
 
 
 
 // const express = require('express');
-// const fetch = require('node-fetch');
 // const path = require('path');
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// // Serve the static files from the React app
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// // Handles any requests that don't match the ones above
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+
+
+
+
+// const express = require('express');
+// const cors = require('cors');
+// const axios = require('axios');
+
+// const app = express();
+// const port = 5000;
+
+// app.use(cors());
+
+// app.get('/api/countries/:name', async (req, res) => {
+//   const countryName = req.params.name;
+
+//   try {
+//     const response = await axios.get(`https://restcountries.com/v3.1/name/${countryName}`);
+//     const data = response.data;
+
+//     if (data.length > 0) {
+//       res.json(data[0]);
+//     } else {
+//       res.status(404).json({ error: 'Country not found' });
+//     }
+//   } catch (error) {
+//     console.error('Error fetching country data:', error);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+
+
+
+// const express = require('express');
 // const app = express();
 // const PORT = process.env.PORT || 3000;
 
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, 'client')));
-
-// app.get('/country/:name', async (req, res) => {
-//     const { name } = req.params;
-
-//     try {
-//         const response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
-//         const data = await response.json();
-
-//         if (data.length > 0) {
-//             res.json(data[0]);
-//         } else {
-//             res.status(404).json({ error: 'Country not found' });
-//         }
-//     } catch (error) {
-//         console.error('Error fetching country data:', error);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// });
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+// app.use(express.static('public'));
 
 // app.listen(PORT, () => {
 //     console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const cors = require('cors');
+// const fetch = require('node-fetch');
+
+// const app = express();
+// const port = 5000;
+
+// app.use(cors());
+
+// app.get('/api/countries/:name', async (req, res) => {
+//   const countryName = req.params.name;
+
+//   try {
+//     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+//     const data = await response.json();
+
+//     if (data.length > 0) {
+//       res.json(data[0]);
+//     } else {
+//       res.status(404).json({ error: 'Country not found' });
+//     }
+//   } catch (error) {
+//     console.error('Error fetching country data:', error);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const cors = require('cors');
+// // const fetch = require('node-fetch');
+
+// const app = express();
+// const port = 5000;
+
+// app.use(cors());
+
+// app.get('/api/countries/:name', async (req, res) => {
+//   const countryName = req.params.name;
+
+//   try {
+//     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+//     const data = await response.json();
+
+//     if (data.length > 0) {
+//       res.json(data[0]);
+//     } else {
+//       res.status(404).json({ error: 'Country not found' });
+//     }
+//   } catch (error) {
+//     console.error('Error fetching country data:', error);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
 // });
--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
-import './App.css'; 
+import './App.css';
 
 function App() {
   const [countryName, setCountryName] = useState('');
   const [countryInfo, setCountryInfo] = useState(null);
 
   const searchCountry = async () => {
-    // Fetch country data from the REST Countries API
     try {
       const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
       const data = await response.json();
 
-      // Update state with country information
       if (data.length > 0) {
-        setCountryInfo(data[0]);
+        const { name, flags, capital, population, languages } = data[0];
+        setCountryInfo({
+          name: name.common,
+          flag: flags.png,
+          capital: capital,
+          population: population.toLocaleString(),
+          languages: languages ? Object.values(languages).join(', ') : 'N/A',
+        });
       } else {
         setCountryInfo(null);
       }
@@ -22,7 +27,6 @@ function App() {
       setCountryInfo(null);
     }
   };
-  
 
   return (
     <div className="container">
@@ -39,8 +43,8 @@ function App() {
       <div id="displayArea">
         {countryInfo ? (
           <div>
-            <h2>{countryInfo.name.common}</h2>
-            <img src={countryInfo.flags.png} alt={`${countryInfo.name.common} flag`} width="100" />
+            <h2>{countryInfo.name}</h2>
+            <img src={countryInfo.flag} alt={`${countryInfo.name} flag`} width="100" />
             <table>
               <tbody>
                 <tr>
@@ -49,11 +53,11 @@ function App() {
                 </tr>
                 <tr>
                   <td>Population:</td>
-                  <td>{countryInfo.population.toLocaleString()}</td>
+                  <td>{countryInfo.population}</td>
                 </tr>
                 <tr>
                   <td>Languages:</td>
-                  <td>{countryInfo.languages.join(', ')}</td>
+                  <td>{countryInfo.languages}</td>
                 </tr>
               </tbody>
             </table>
@@ -67,11 +71,4 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
 
